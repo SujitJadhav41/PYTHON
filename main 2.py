01b50d8d217b799c3bb44069e1c5d55bbdd511df
Sujit Mahadev Jadhav <161117944+SujitJadhav41@users.noreply.github.com>
2026-07-18 12:43:04 +0530

    Add files via upload

---
+++ main 2.py
@@ -0,0 +1,8 @@
+name = input("Whats your name: ")
+work = input("Where do you work: ")
+live = input("Where do you live?: ")
+
+# Harry works at CodeWithHarry and lives in Delhi
+# print(name + " works at " + work + " and lives in " + live) 👎
+
+print(f"{name} works at {work} and lives in {live}")
